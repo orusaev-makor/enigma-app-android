4eaed416af06d36da2f91903c929596bd0e7540d
orusaev-makor <72498070+orusaev-makor@users.noreply.github.com>
2020-11-26 18:05:38 +0200

    Chart pie and grouped bars

--- app/src/main/java/com/example/enigmaapp/BalanceFragment.java
+++ app/src/main/java/com/example/enigmaapp/BalanceFragment.java
@@ -1,5 +1,6 @@
 package com.example.enigmaapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link BalanceFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class BalanceFragment extends Fragment {
+
+    PieChart pieChart;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +71,42 @@ public class BalanceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_balance, container, false);
+        View v = inflater.inflate(R.layout.fragment_balance, container, false);
+
+        pieChart = (PieChart) v.findViewById(R.id.balancePieChart);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10, 5, 5);
+
+        pieChart.setDragDecelerationFrictionCoef(0.15f);
+
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> yValues = new ArrayList<>();
+
+        yValues.add(new PieEntry(34f, "SA"));
+        yValues.add(new PieEntry(23f, "USA"));
+        yValues.add(new PieEntry(14f, "UK"));
+        yValues.add(new PieEntry(35f, "India"));
+        yValues.add(new PieEntry(40f, "Russia"));
+        yValues.add(new PieEntry(23f, "Japan"));
+
+        // animate pie on creation:
+//        pieChart.animateY(1000);
+
+        PieDataSet dataSet = new PieDataSet(yValues, "Countries");
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData(dataSet);
+        data.setValueTextSize(10f);
+        data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data)
+        ;
+        return v;
     }
 }
--- app/src/main/java/com/example/enigmaapp/StatisticsFragment.java
+++ app/src/main/java/com/example/enigmaapp/StatisticsFragment.java
@@ -1,5 +1,6 @@
 package com.example.enigmaapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,12 +9,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+
+import java.util.ArrayList;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link StatisticsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class StatisticsFragment extends Fragment {
+
+    BarChart barChart;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,10 +67,81 @@ public class StatisticsFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistics, container, false);
+        View v = inflater.inflate(R.layout.fragment_statistics, container, false);
+
+        barChart = (BarChart) v.findViewById(R.id.statisticsBarChart);
+
+        BarDataSet barDataSet1 = new BarDataSet(barEntries1(), "Nominal (1,000$)");
+        barDataSet1.setColor(Color.rgb(50, 171, 209));
+        barDataSet1.setValueTextSize(0f);
+
+        BarDataSet barDataSet2 = new BarDataSet(barEntries2(), "Trade Count (Unit)");
+        barDataSet2.setColor(Color.rgb(175, 89, 212));
+        barDataSet2.setValueTextSize(0f);
+
+        BarData data = new BarData(barDataSet1, barDataSet2);
+        barChart.setData(data);
+
+        String[] years = new String[]{"2017", "2018", "2019", "2020"};
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(years));
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setGranularity(1);
+        xAxis.setCenterAxisLabels(true);
+        xAxis.setAxisMaximum(1);
+//        xAxis.setGranularityEnabled(true);
+
+        barChart.setDrawBarShadow(false);
+        barChart.setDrawValueAboveBar(false);
+
+//        mpBarChart.setDescription("");
+        barChart.getDescription().setEnabled(false);
+        barChart.setMaxVisibleValueCount(50);
+        barChart.setPinchZoom(false);
+        barChart.setDrawGridBackground(false);
+
+        barChart.setDragEnabled(true);
+        barChart.setVisibleXRangeMaximum(50);
+
+        float groupSpace = 0.28f;
+        float barSpace = 0.02f;
+        float barWidth = 0.16f;
+        data.setBarWidth(barWidth);
+
+
+        //IMPORTANT *****
+        barChart.getXAxis().setAxisMaximum(0);
+        barChart.getXAxis().setAxisMaximum(0 + barChart.getBarData().getGroupWidth(groupSpace, barSpace) * 4); // 4 is the number of bars
+        barChart.getAxisLeft().setAxisMinimum(0);
+        barChart.groupBars(0, groupSpace, barSpace);  // perform the "explicit" grouping
+        //***** IMPORTANT
+
+        barChart.invalidate();
+
+        return v;
     }
+
+    private ArrayList<BarEntry> barEntries1() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 20));
+        barEntries.add(new BarEntry(2, 15));
+        barEntries.add(new BarEntry(3, 45));
+        barEntries.add(new BarEntry(4, 30));
+        return barEntries;
+    }
+
+    private ArrayList<BarEntry> barEntries2() {
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(1, 22));
+        barEntries.add(new BarEntry(2, 30));
+        barEntries.add(new BarEntry(3, 25));
+        barEntries.add(new BarEntry(4, 50));
+        return barEntries;
+    }
+
 }
