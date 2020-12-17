7ca4bcd92266e0dc72f626ca105cfe1093989040
orusaev-makor <72498070+orusaev-makor@users.noreply.github.com>
2020-12-17 16:07:19 +0200

    fixed issue with bullet points colour

--- app/src/main/java/com/example/enigmaapp/ui/SettlementItemAdapter.java
+++ app/src/main/java/com/example/enigmaapp/ui/SettlementItemAdapter.java
@@ -1,7 +1,9 @@
 package com.example.enigmaapp.ui;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
@@ -29,13 +32,11 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
     private static final DiffUtil.ItemCallback<SettlementRepository.SettlementSummary> DIFF_CALLBACK = new DiffUtil.ItemCallback<SettlementRepository.SettlementSummary>() {
         @Override
         public boolean areItemsTheSame(@NonNull SettlementRepository.SettlementSummary oldItem, @NonNull SettlementRepository.SettlementSummary newItem) {
-//            return true;
             return oldItem.getId() == newItem.getId() && oldItem.getBatchId() == newItem.getBatchId();
         }
 
         @Override
         public boolean areContentsTheSame(@NonNull SettlementRepository.SettlementSummary oldItem, @NonNull SettlementRepository.SettlementSummary newItem) {
-//            return false;
             return oldItem.getId() == newItem.getId() &&
                     oldItem.getBatchId() == newItem.getBatchId() &&
                     oldItem.getDate().equals(newItem.getDate()) &&
@@ -58,6 +59,7 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
     public void onBindViewHolder(@NonNull ItemHolder holder, int position) {
         SettlementRepository.SettlementSummary currentBatchItem = getItem(position);
 
+        // check if show Batch ID:
         if (currentBatchItem.isBatch()) {
             holder.textViewBatchId.setText("ID " + currentBatchItem.getBatchId());
             holder.textViewBatchId.setVisibility(View.VISIBLE);
@@ -68,16 +70,14 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
         holder.textViewProduct.setText(currentBatchItem.getName());
         holder.textViewCounterparty.setText(currentBatchItem.getCounterparty());
 
-        // remove time from "sent at"
+        // remove time from "sent at" and set the date:
         String date = currentBatchItem.getDate().substring(0, currentBatchItem.getDate().indexOf(" "));
         holder.textViewSentAt.setText(date);
 
+        // set status and it's colour:
         String status = currentBatchItem.getStatus();
         holder.textViewStatus.setText(status);
-        System.out.println("STATUS: " + status);
-
         int statusColor = 0;
-
         switch (status) {
             case "pending":
                 statusColor = context.getResources().getColor(R.color.statusPending);
@@ -98,9 +98,17 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
                 break;
         }
         holder.textViewStatus.setTextColor(statusColor);
-        Drawable unwrappedDrawable = AppCompatResources.getDrawable(context, R.drawable.ic_bullet_point);
+
+        // set bullet point by status colour:
+        Drawable unwrappedDrawable = ContextCompat.getDrawable(context, R.drawable.ic_bullet_point).mutate();
         Drawable wrappedDrawable = DrawableCompat.wrap(unwrappedDrawable);
         DrawableCompat.setTint(wrappedDrawable, statusColor);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            holder.bulletPoint.setBackground(wrappedDrawable);
+        } else {
+            holder.bulletPoint.setBackgroundDrawable(wrappedDrawable);
+        }
     }
 
     class ItemHolder extends RecyclerView.ViewHolder {
@@ -109,6 +117,7 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
         private TextView textViewCounterparty;
         private TextView textViewSentAt;
         private TextView textViewStatus;
+        private View bulletPoint;
 
         public ItemHolder(View itemView) {
             super(itemView);
@@ -117,6 +126,8 @@ public class SettlementItemAdapter extends ListAdapter<SettlementRepository.Sett
             textViewCounterparty = itemView.findViewById(R.id.settlement_item_counterparty);
             textViewSentAt = itemView.findViewById(R.id.settlement_item_sent_at);
             textViewStatus = itemView.findViewById(R.id.settlement_item_status);
+            bulletPoint = itemView.findViewById(R.id.settlement_item_bullet_point);
+
 
             itemView.setOnClickListener(v -> {
                 int position = getAdapterPosition();
