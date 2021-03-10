b0e0efc44d7e874514d35ef3dd2211a7e4656ad2
orusaev-makor <72498070+orusaev-makor@users.noreply.github.com>
2021-03-10 19:26:56 +0200

    change password screen

---
+++ app/src/main/java/com/example/enigmaapp/activity/fragment/ChangePasswordFragment.java
@@ -0,0 +1,41 @@
+package com.example.enigmaapp.activity.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.enigmaapp.R;
+import com.google.android.material.button.MaterialButton;
+
+public class ChangePasswordFragment extends Fragment {
+    private MaterialButton saveBtn;
+
+    public ChangePasswordFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_change_password, container, false);
+
+        saveBtn = v.findViewById(R.id.profile_new_password_btn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("saving new password....");
+            }
+        });
+        return v;
+    }
+}
