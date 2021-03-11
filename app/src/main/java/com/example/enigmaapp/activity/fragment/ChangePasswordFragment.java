ea29bf30713c577548dbb259ef1d052d6b71927a
orusaev-makor <72498070+orusaev-makor@users.noreply.github.com>
2021-03-11 15:25:22 +0200

    change pass focusable fields conditioned

--- app/src/main/java/com/example/enigmaapp/activity/fragment/ChangePasswordFragment.java
+++ app/src/main/java/com/example/enigmaapp/activity/fragment/ChangePasswordFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.enigmaapp.R;
 import com.google.android.material.button.MaterialButton;
 
+import java.util.HashMap;
+
 public class ChangePasswordFragment extends Fragment {
     private MaterialButton saveBtn;
+    private EditText currentPass, newPass, newPassRetype;
+    private HashMap<String, String> changePassParams = new HashMap<>();
 
     public ChangePasswordFragment() {
         // Required empty public constructor
@@ -29,12 +36,34 @@ public class ChangePasswordFragment extends Fragment {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_change_password, container, false);
 
-        saveBtn = v.findViewById(R.id.change_pass_save_btn);
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        currentPass = v.findViewById(R.id.change_pass_current_pass_edit_text);
+        newPass = v.findViewById(R.id.change_pass_new_pass_edit_text);
+        newPassRetype = v.findViewById(R.id.change_pass_new_pass_retype_edit_text);
+        currentPass.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v) {
-                System.out.println("saving new password....");
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
             }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // allows focus on other fields only if old password field is not empty:
+                newPass.setFocusable(!s.toString().equals(""));
+                newPassRetype.setFocusable(!s.toString().equals(""));
+                newPass.setFocusableInTouchMode(!s.toString().equals(""));
+                newPassRetype.setFocusableInTouchMode(!s.toString().equals(""));
+            }
+        });
+
+
+        saveBtn = v.findViewById(R.id.change_pass_save_btn);
+        saveBtn.setOnClickListener(v1 -> {
+            changePassParams.put("old_password", currentPass.getText().toString());
+            changePassParams.put("new_password", newPass.getText().toString());
+            changePassParams.put("new_password_retype", newPassRetype.getText().toString());
         });
         return v;
     }
